a3e098c7e5561d7db4902e7ac863d226e844ffde
shantanu-mastek <shantanu.charpe@mastek.com>
2014-10-17 16:11:44 +0530

    Added missing package name
    Added missing package name

--- supporting_libraries/stg-commons/src/main/java/com/stg/logger/LoggingOutputStream.java
+++ supporting_libraries/stg-commons/src/main/java/com/stg/logger/LoggingOutputStream.java
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with JBEAM. If not, see <http://www.gnu.org/licenses/>.
  */
+package com.stg.logger;
 
 import java.io.*;
 import org.apache.log4j.*;
